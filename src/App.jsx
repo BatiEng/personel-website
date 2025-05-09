d803e6308c8d7da0847963970e87d462129d439f
BatiEng <alibatikan@gmail.com>
2025-05-10 00:47:50 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Skills from "./sections/Skills";
 import Contact from "./sections/Contact";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
